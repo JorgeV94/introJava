04fb37e07f3c5e9edd3240f4bf3fc41eca9535d3
Jorge Vieira <jorge.vieira.a@hotmail.com>
2023-02-28 16:32:57 -0300

    Inicio do projeto

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,45 @@
+// 1 - Pacote
+package intro;
+
+
+// 2 - Referência as bibliotecas
+
+import java.sql.SQLOutput;
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 - Atributos - Caracteristicas
+
+    // 3.2 - Métodos e Funções
+    public static void main(String[] args){
+        System.out.println("Calculo de Areas");
+
+    }
+
+    public void calcularAreaModoExtenso{
+
+        // Calculo de area - Exemplo: o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 4;
+        comprimento = 3;
+
+        resultado = largura * comprimento;
+
+        System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento + "m a area é de " +resultado+ "m2");
+
+    }
+
+
+    public void calcularAreaModoCompacto(){
+
+        //Calculo de area Reduzido
+        int largura = 4;
+        int comprimento = 3;
+
+        System.out.println("Para a largura de " + largura + "m e o comprimento de " + comprimento + "m a area é de " + largura*comprimento + "m2");
+    }
+
+}
